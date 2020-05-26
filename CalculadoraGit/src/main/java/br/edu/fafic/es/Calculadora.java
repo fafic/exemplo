3e832b635ba7451b1fd2e1927fef13304228fda2
Luciano Tavares <lucianotavares@univs.edu.br>
2020-05-26 18:02:32 -0300

    alterando multiplicar novamente

--- CalculadoraGit/src/main/java/br/edu/fafic/es/Calculadora.java
+++ CalculadoraGit/src/main/java/br/edu/fafic/es/Calculadora.java
@@ -12,8 +12,8 @@ package br.edu.fafic.es;
 public class Calculadora {
     
 
-    public int multiplicar(int a, int b, int c){
-        return a*b*c;
+    public int multiplicar(int a, int b){
+        return a*b;
     }
     
     public int somar(int a, int b){
